46fe6f9b7dd3bc09557819adc07ce0541a6df7c0
David Nguyen <hpcsc@users.noreply.github.com>
2019-10-09 10:03:22 +1100

    allow specifying location in key uploading script

--- scripts/upload-public-key.sh
+++ scripts/upload-public-key.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 KEY_NAME=${1:-default-key}
+KEY_LOCATION=${2:-~/.ssh/id_rsa.pub}
 
 source ./scripts/_read_user_credentials.sh
 
@@ -18,7 +19,7 @@ if [[ -z "${EXISTING_KEY}" ]]; then
         -X POST \
         -H "Content-Type: application/json" \
         -H "Authorization: token ${TOKEN}" \
-        -d "{ \"title\": \"${KEY_NAME}\", \"key\": \"$(cat ~/.ssh/id_rsa.pub)\" }" \
+        -d "{ \"title\": \"${KEY_NAME}\", \"key\": \"$(cat ${KEY_LOCATION})\" }" \
         http://localhost:3000/api/v1/user/keys
 else
     echo "=== Key ${KEY_NAME} is uploaded before"
